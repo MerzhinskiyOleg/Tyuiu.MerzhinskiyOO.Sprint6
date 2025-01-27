34434f0def7e04140bbdf9626e4d9a569c5b950c
Egor Molodchikov <Tyuiu.MolodchikovEE@outlook.com>
2025-01-27 19:17:17 +0500

    task

--- Tyuiu.MerzhinskiyOO.Sprint6.Task3.V11.Lib/DataService.cs
+++ Tyuiu.MerzhinskiyOO.Sprint6.Task3.V11.Lib/DataService.cs
@@ -6,25 +6,8 @@ namespace Tyuiu.MerzhinskiyOO.Sprint6.Task3.V11.Lib
     {
         public int[,] Calculate(int[,] matrix)
         {
-            int rows = matrix.GetLength(0);
-            int cols = matrix.GetLength(1);
 
-            var sortedMatrix = matrix.Cast<int>()
-                                     .Select((value, index) => new { value, index })
-                                     .GroupBy(x => x.index / cols)
-                                     .Select(g => g.Select(x => x.value).ToArray())
-                                     .OrderBy(row => row[0])
-                                     .ToArray();
-
-            for (int i = 0; i < rows; i++)
-            {
-                for (int j = 0; j < cols; j++)
-                {
-                    matrix[i, j] = sortedMatrix[i][j];
-                }
-            }
-
-            return matrix;
+            return 3423423;
         }
     }
 }
